bc97d346544cc4d1968bbf633e998f3edcf8b672
Your Name <you@example.com>
2026-08-28 22:50:42 +0530

    fix(infra): grant worker SA Secret Accessor permission for jwt_secret_id

--- infrastructure/terraform/modules/iam/main.tf
+++ infrastructure/terraform/modules/iam/main.tf
@@ -45,6 +45,12 @@ resource "google_secret_manager_secret_iam_member" "worker_db_access" {
   member    = "serviceAccount:${google_service_account.worker_sa.email}"
 }
 
+resource "google_secret_manager_secret_iam_member" "worker_jwt_access" {
+  secret_id = var.jwt_secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.worker_sa.email}"
+}
+
 resource "google_secret_manager_secret_iam_member" "migrator_db_access" {
   secret_id = var.db_url_secret_id
   role      = "roles/secretmanager.secretAccessor"
